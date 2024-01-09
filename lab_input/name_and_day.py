aca9daa5454cdac12f2f2a4a91ed359ec304a2ac
Titus Kovalik <titus-kovalik@outlook.com>
2024-01-09 23:14:10 +0000

    mixed lists

---
+++ lab_input/name_and_day.py
@@ -0,0 +1,5 @@
+def main():
+    name = input("What is your name? ")
+    day = input("What is the day today? ")
+    print(f"Hello {name}, Happy {day}!")
+main()
---
+++ mix-list/mixlist01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+# making a list contain 3 things
+my_list = [ "192.168.0.5", 5060, "UP" ]
+# display first item
+print("The first item of the list (IP): " + my_list[0])
+# display second item
+print("The second item of the list (port): " + str(my_list[1]))
+# display second item 
+print("The third item of the list (state): " + my_list[2])
