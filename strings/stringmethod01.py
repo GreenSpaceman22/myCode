d42d7fbc79177523da9e08eac5eafb1b71ed1712
Titus Kovalik <titus-kovalik@outlook.com>
2024-01-09 22:41:58 +0000

    join and split

---
+++ strings/stringmethod01.py
@@ -0,0 +1,9 @@
+lilstring = "Alta3 Research offers classes on Python coding"
+newlist = lilstring.split(" ")
+print(newlist)
+myiplist = ["192", "168", "0", "12"]
+# set singleip as the result of joining the list myiplist around the "."
+singleip = ".".join(myiplist)
+# display singleip
+print(singleip)
+
