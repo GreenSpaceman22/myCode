09620222a8e181342024dc94f561e0a124064d4a
Titus Kovalik <titus-kovalik@outlook.com>
2024-01-10 22:23:14 +0000

    added lab 44, testing with if

---
+++ iftest/condition01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+hostname = "MTG"
+hostname = hostname.upper()
+
+if hostname == "MTG":
+    print("The hostname was found to be MTG")
+    
---
+++ iftest/condition03.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+hostname = input("What value should we set for hostname?")
+hostname = hostname.upper()
+
+
+if hostname == "MTG":
+    print("The hostname was found to be MTG")
+    print("hostname matches expected config")
+
+print("Exiting the script")
+
