4f6e65923c41b7d091cb2fe171c5555e6a3dbaee
Titus Kovalik <titus-kovalik@outlook.com>
2024-01-10 21:37:39 +0000

    added lab 42 with nightmare challenge

---
+++ dict01/dictionary_chlng.py
@@ -0,0 +1,18 @@
+challenge= ["science", "turbo", ["goggles", "eyes"], "nothing"]
+
+
+trial= ["science", "turbo", {"eyes": "goggles", "goggles": "eyes"}, "nothing"]
+
+
+nightmare= [{"slappy": "a", "text": "b", "kumquat": "goggles", "user":{"awesome": "c", "name": {"first": "eyes", "last": "toes"}},"banana": 15, "d": "nothing"}]
+
+print(f"My {challenge[2][1]}! The {challenge[2][0]} do {challenge[3]}!")
+
+print(f"My {trial[2].get('goggles')}! The {trial[2].get('eyes')} do {trial[3]}!")
+
+eyes = nightmare[0].get('user')
+eyes = eyes.get("name")
+eyes = eyes.get("first")
+
+print(f"My {eyes}! The {nightmare[0].get('kumquat')} do {nightmare[0].get('d')}!")
+
