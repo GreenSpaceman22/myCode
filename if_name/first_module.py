f0677694dd6b6a49d8d589b310df5e766e5c4816
Titus Kovalik <titus-kovalik@outlook.com>
2024-01-12 20:58:36 +0000

    cheater dice game completed, intro to oop and classes

---
+++ if_name/first_module.py
@@ -0,0 +1,13 @@
+print("this code will always run")
+
+
+def main():
+    print("This code belongs to the main function in module 1.")
+    
+if __name__ == "__main__":
+
+    main()
+
+
+
+
---
+++ if_name/second_module.py
@@ -0,0 +1,3 @@
+import first_module
+
+print("Modue #2 Name=", __name__)
